23df439470f656aa90bc3076978522db989dbcb1
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-02-17 12:07:41 -0800

    whitspace removal

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,6 @@ int main(void)
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
 				putchar((num3 % 10) + '0');
-
 				if (num1 == 7 && num2 == 8 && num3 == 9)
 					continue;				
 				putchar(',');
@@ -25,8 +24,6 @@ int main(void)
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
