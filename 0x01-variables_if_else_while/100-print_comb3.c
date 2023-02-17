53ab40ce6c05651168476d5b367e017a3f81f9c9
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-02-17 11:22:18 -0800

    Adding 00-print_comb3.c file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,17 @@
  */
 int main(void)
 {
-	int m;
+	int d1, d2;
 
-	for (m = 0; m < 100; m++)
+	for (d1 = 0; d1 < 9; d1++)
 	{
-		putchar((m / 10) + '0');
-		putchar((m % 10) + '0');
-		if (m != 99)
+		for (d2 = d1 + 1; d2 < 10; d2++)
 		{
+			putchar((d1 % 10) + '0');
+			putchar((d2 % 10) + '0');
+
+			if (d1 == 8 && d2 == 9)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
