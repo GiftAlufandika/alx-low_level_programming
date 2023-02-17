8761a8e31a108e4c76e0d41ff399bd91ea385f69
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-02-17 05:39:46 -0800

    Whitespaces fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -16,6 +15,5 @@ int main(void)
 		putchar(low);
 	}
 		putchar('\n');
-	
 	return (0);
 }
