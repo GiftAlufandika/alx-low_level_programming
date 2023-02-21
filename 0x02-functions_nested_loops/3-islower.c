0b694da838fab1687b162d6ea675dd952d132de0
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-02-21 12:02:17 -0800

    Adding 3-islower file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  * _islower - a function that checks for lowercase character
- * k: single letter input
+ * @k: single letter input
  * Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _islower(int k)
@@ -11,9 +11,8 @@ int _islower(int k)
 		return (1);
 
 	}
-	else 
+	else
 	{
 		return (0);
 	}
 }
-
