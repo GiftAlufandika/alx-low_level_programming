9b0fbf7eceffe3de4777d06049460163d5be3bd8
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-02-21 07:22:44 -0800

    Betty erro fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 /*Include main header file*/
 #include "main.h"
 /**
- * Main - Prints _putchar followed by a new line
+ * main - Prints _putchar followed by a new line
  *
  * Return: Always 0
  */
+
 int main(void)
 {
 	char *text = "_putchar";
