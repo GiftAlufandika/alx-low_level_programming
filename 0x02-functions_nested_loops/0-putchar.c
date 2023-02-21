036b5d0ca0e5791cd839d2bbf7dfa62021d247a7
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-02-21 06:42:35 -0800

    Adding 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * Main  - Entry point
+ *
+ * Return - A;lways 0
+ *
+ */
+ int main(void)
+{
+	char *text = "_putchar";
+
+	while (*text)
+	{
+		_putchar(*text);
+		text++;
+	}
+	_putchar('\n');
+	return (0);
+}
