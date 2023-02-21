987d876ea92d9050cbbbeabc4b0d2f9f299c9d08
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-02-21 08:32:37 -0800

    Adding  2-alphabet_x10.c file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,10 @@
 void print_alphabet_x10(void)
 {
 	int count, alph;
+
 	for (count = 1; count <= 10; count++)
 	{
-		for (alph = 'a'; alph < 'z'; alph++)
+		for (alph = 'a'; alph <= 'z'; alph++)
 		{
 			_putchar(alph);
 		}
