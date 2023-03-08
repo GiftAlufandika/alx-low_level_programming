3a3917036dff5b8acf46ca577a58fbaffc2f8e09
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-03-08 11:21:07 -0800

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -40,3 +40,28 @@ int divisible(int my_num, int divisor)
 		return (divisible(my_num, divisor + 1));
 	}
 }
+
+/**
+ * is_prime_number - function definition
+ * @n: the number to be tested
+ * Description: find a prime number
+ * Return: 0 for not prime, 1 for prime
+ */
+
+int is_prime_number(int n)
+{
+	int divisor = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n >= 2 && n <= 3)
+	{
+		return (1);
+	}
+	else
+	{
+		return (divisible(n, divisor));
+	}
+}
