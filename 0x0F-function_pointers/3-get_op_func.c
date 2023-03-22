1147d79e2b5cc65808e97622ae7cfaee48b441dc
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-03-22 12:47:21 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,67 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include"3-calc.h"
 /**
- * op_add - addition operator
- * @a: first int
- * @b: second int
- * Return: sum of 2 numbers
- */
-int op_add(int a, int b)
-{
-	return (a + b);
-}
+ * get_op_func - get the operator and redirect to the func.
+ * @s: the operator
+(* a blank line
+* Description: get the operator and redirect to the func.)?
+(* section header: 3-calc.h)*
+* Return: return null or the function to do.
+*/
 
-/**
- * op_sub - subtraction operator
- * @a: first in
- * @b: second int
- * Return: difference of 2 numbers
- */
-int op_sub(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a - b);
-}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-/**
- * op_mul - product of 2 numbers
- * @a: first int
- * @b: second int
- * Return: product of the numbers
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - division of 2 numbers
- * @a: first int
- * @b: second int
- * Return: result of the division of the numbers
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
+	i = 0;
+	while (i < 5)
 	{
-		printf("Error\n");
-		exit(100);
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (*(ops[i]).f);
+		}
+		i++;
 	}
-	return (a / b);
-}
-
-/**
- * op_mod - get remainder of the division of 2 numbers
- * @a: first int
- * @b: second int
- * Return: remainder of division of the numbers
- */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+	return (NULL);
 }
