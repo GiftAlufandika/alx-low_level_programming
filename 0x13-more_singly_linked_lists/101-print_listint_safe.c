ff35014c604b3d4efc24e4bd3da7789c848356c4
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-04-04 15:17:19 -0700

    Hot fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,45 +7,50 @@
  *
  * Return: Length of list (INT)
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *slow_p = head, *fast_p = head;
-    size_t ele = 0;
-    int is_loop = 0;
+	const listint_t *slow_p = head ,*fast_p = head;
+	size_t ele = 0;
+	int is_loop = 0;
 
-    for (; slow_p && fast_p && fast_p->next; slow_p = slow_p->next, fast_p = fast_p->next->next)
-    {
-        if (!(fast_p->next->next))
-            break;
-        if (slow_p == fast_p)
-        {
-            slow_p = slow_p->next;
-            is_loop = 1;
-            break;
-        }
-    }
+	while (slow_p && fast_p && fast_p->next)
+	{
+		if (!(fast_p->next->next))
+			break;
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+		if (slow_p == fast_p)
+		{
+			slow_p = slow_p->next;
+			is_loop = 1;
+			break;
+		}
+	}
 
-    if (!is_loop)
-    {
-        for (; head; head = head->next, ele++)
-        {
-            printf("[%p] %d\n", (void *)head, head->n);
-        }
-        return (ele);
-    }
+	if (!is_loop)
+	{
+		while (head)
+		{
+			ele++;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		return (ele);
+	}
 
-    for (; head; head = head->next, ele++)
-    {
-        if (head == slow_p)
-        {
-            printf("[%p] %d\n", (void *)head, head->n);
-            printf("-> [%p] %d\n", (void *)head, head->next->n);
-            exit(98);
-        }
-        printf("[%p] %d\n", (void *)head, head->n);
-    }
+	while (head)
+	{
+		ele++;
+		if (head == slow_p)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			printf("-> [%p] %d\n", (void *)head, head->next->n);
+			exit(98);
+		}
 
-    return (0);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+	return (0);
 }
 
