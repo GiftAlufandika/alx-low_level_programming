2609dd0ad2d1739e61a5818dda5d268e0a45085c
Gift Alufandika <bsc-10-18@unima.ac.mw>
2023-04-09 16:02:50 -0700

    Adding all files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: pointer to the name of the file.
+ * @text_content:  pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int open_f, write_f, length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+		{
+			length++;
+		}
+	}
+
+	open_f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write_f = write(op, text_content, length);
+
+	if (open_f == -1 || write_f == -1)
+		return (-1);
+
+	close(open_f);
+
+	return (1);
+}
